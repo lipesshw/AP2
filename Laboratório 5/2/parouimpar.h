73ab5efb3c6de54439b723e04e97be9513b4b4fd
felipe7 <lipebroken@gmail.com>
2023-11-27 10:37:32 -0300

    Create parouimpar.h

---
+++ Laboratório 5/2/parouimpar.h
@@ -0,0 +1,12 @@
+#ifndef PAROUIMPAR_H_INCLUDED
+#define PAROUIMPAR_H_INCLUDED
+
+int Positivo_Negativo(int numero) {
+    if (numero > 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+#endif // PAROUIMPAR_H_INCLUDED
