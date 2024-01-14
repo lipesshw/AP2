2d15e119081754cbff1376cc431e97f7f92c8366
felipe7 <lipebroken@gmail.com>
2024-01-14 04:10:00 -0300

    Update 8.c

--- Laboratório 6/8/8.c
+++ Laboratório 6/8/8.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include "tribonacci.h"
 
+
 int main() {
     int n;
 
-    printf("Digite o valor de n para calcular o número Tribonacci: ");
+    printf("Digite o valor de N para calcular o N-ésimo número Tribonacci: ");
     scanf("%d", &n);
 
     if (n < 0) {
-        printf("O valor de n deve ser não-negativo.\n");
+        printf("O valor de N deve ser não-negativo.\n");
     } else {
         int resultado = tribonacci(n);
-        printf("O %dº número Tribonacci é: %d\n", n, resultado);
+        printf("O %d-ésimo número Tribonacci é: %d\n", n, resultado);
     }
 
     return 0;
