67d42e3fafc721beeb897f0b0b17bf16de6ff14e
felipe7 <lipebroken@gmail.com>
2024-01-22 10:30:42 -0300

    Create 7.c

---
+++ Laboratório 8/7/7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+int i=3, j=5;
+int *p, *q;
+p = &i;
+q = &j;
+
+
+Qual é o valor das seguintes expressões?
+a) p == &i; sim
+
+b) *p - *q; -2
+
+c)*&p; 3
+
+d)3* - *p/(*q)+7; 5.2
+
+return 0;
+}
+
